57bf4838a3579374246d8c7b275c514df3bd11a3
yeonho <yeonho@in-soft.co.kr>
2022-04-27 12:49:14 +0900

    [common] 표준 프로젝트 설정 - 프로젝트 기본 구조 설정 - React Bootstrap 18 버전으로 변경.

---
+++ src/views/App.js
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import About from './about/About'
+import Home from './home/Home'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
