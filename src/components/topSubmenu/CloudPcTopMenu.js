4017201cc2388c4f4438abce0ceddcea0096fb7f
bp_yeonho <yeonho@in-soft.co.kr>
2022-05-14 22:23:38 +0900

    [feature] CloudPcTopMenu 디자인 완료

--- src/components/topSubmenu/CloudPcTopMenu.js
+++ src/components/topSubmenu/CloudPcTopMenu.js
@@ -1,21 +1,35 @@
 import React from 'react'
-import { Space, Button, Row, Col } from 'antd'
+import { Space, Button, Row, Col, Divider } from 'antd'
 
 const CloudPcTopMenu = () => {
+  const style = {
+    rowRight: {
+      float: 'right'
+    },
+    button: {
+      width: '146px',
+      fontWeight: '600'
+    },
+    divider: {
+      marginTop: '60px',
+      borderBottom: '1px solid'
+    }
+  }
   return (
     <>
-      <Row>
+      <Row style={style.rowRight}>
         <Col>
-          <Button className="width-150" type="primary" size="large">
-            개인 디스크 관리
-          </Button>
-        </Col>
-        <Col>
-          <Button className="width-150" type="primary" size="large">
-            PC 순서 조정
-          </Button>
+          <Space size="large">
+            <Button style={style.button} size="large">
+              개인 디스크 관리
+            </Button>
+            <Button style={style.button} size="large">
+              PC 순서 조정
+            </Button>
+          </Space>
         </Col>
       </Row>
+      <Divider style={style.divider} />
     </>
   )
 }
